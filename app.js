fa3d640744ac508c75e57d1f515e63247a826c4b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-27 12:43:19 -0700

    cambios finales

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const buttons = document.querySelectorAll('button');
 
 const playnote = event=> 
+
 {
     const button = event.target;
     const note = button.dataset.note;
-    const audioId = `audio${note}`
-    console.log(audioId);
+    const audioId = `audio${note}`;
+    //console.log(audioId);
     const audio= document.getElementById(audioId);
+    console.log(audio);
     audio.pause();
     audio.currentTime=0;
     audio.play();
 }
+
 buttons.forEach
 (
     button=> button.addEventListener('click', playnote)
